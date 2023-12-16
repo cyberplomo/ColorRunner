731268a03ba099d5fcb5c84f426dadf4ea01029c
Yigit Yeg <yyeg214@gmail.com>
2023-12-16 18:59:57 +0300

    Character Clones color changes script has been updated. I fixed the problem of Clones color changes problem which is when character change his color the clones color are not changes at the same time

--- Assets/Scripts/ColorSwitch.cs
+++ Assets/Scripts/ColorSwitch.cs
@@ -2,24 +2,43 @@ using UnityEngine;
 
 public class ColorSwitch : MonoBehaviour
 {
+    private Material originalMaterial;
+    private Color newColor;
+
+    private void Start()
+    {
+        // Karakterin orijinal materyalini kaydet
+        originalMaterial = GetComponent<Renderer>().material;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("SwitchColor"))
         {
-            // Karakterin rengini değiştir.
-            Renderer karakterRenderer = GetComponent<Renderer>();
+            // Yeni renk alınacak
+            newColor = other.GetComponent<Renderer>().material.color;
 
-            if (karakterRenderer != null)
+            // Karakterin materyalini değiştir
+            GetComponent<Renderer>().material.color = newColor;
+
+            // Tüm klonların renklerini değiştir
+            ChangeAllClonesColor(newColor);
+        }
+    }
+
+    private void ChangeAllClonesColor(Color newColor)
+    {
+        // Tüm klonları bul
+        GameObject[] clones = GameObject.FindGameObjectsWithTag("PlayerClone");
+
+        // Her klonun rengini değiştir
+        foreach (GameObject clone in clones)
+        {
+            Renderer cloneRenderer = clone.GetComponent<Renderer>();
+            if (cloneRenderer != null)
             {
-                karakterRenderer.material.color = other.GetComponent<Renderer>().material.color;
-            }
-            else
-            {
-                Debug.LogError("Renderer component not found on the Player game object!");
+                cloneRenderer.material.color = newColor;
             }
         }
     }
-    
 }
-
-
